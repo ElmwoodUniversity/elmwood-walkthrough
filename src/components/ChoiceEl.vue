7735e6145a5fb431d1a34ef8a87818d2ba454547
SpedNSFW <spedwards.adult@gmail.com>
2026-09-07 04:41:24 +1000

    add choice backgrounds

--- src/components/ChoiceEl.vue
+++ src/components/ChoiceEl.vue
@@ -25,13 +25,36 @@ const borderStyle = computed(() => {
     background: `linear-gradient(to right, ${colours})`,
   }
 })
+
+const backgroundStyle = computed(() => {
+  const opacity = '5%'
+  if (props.girls.length === 0) {
+    // return { background: '#ffffff0d' }
+    return { background: `rgb(255, 255, 255, ${opacity})` }
+  }
+  const colours = props.girls
+      .map(girl => `rgba(from var(--color-${girl.colour}) r g b / ${opacity})`)
+      .join(', ')
+
+  if (props.girls.length === 1) {
+    return {
+      background: `rgb(from var(--color-${props.girls[0]!.colour}) r g b / ${opacity})`,
+    }
+  }
+
+  return {
+    background: `linear-gradient(to right, ${colours})`,
+  }
+})
 </script>
 
 <template>
   <div class="max-w-md w-md rounded-3xl p-0.5 my-3 mx-2" :style="borderStyle">
-    <div class="rounded-[calc(1.5rem-2px)] p-8 bg-background relative">
-      <p class="font-bold relative" :class="{ '-top-3': choice.note }">{{ choice.optionName }}</p>
-      <p class="text-sm italic absolute bottom-2 pr-8" v-if="choice.note">{{ choice.note }}</p>
+    <div class="rounded-[calc(1.5rem-2px)] bg-background relative">
+      <div class="w-full h-full p-8" :style="backgroundStyle">
+        <p class="font-bold relative" :class="{ '-top-3': choice.note }">{{ choice.optionName }}</p>
+        <p class="text-sm italic absolute bottom-2 pr-8" v-if="choice.note">{{ choice.note }}</p>
+      </div>
       <p class="text-xs absolute top-[-14px] left-5 bg-background p-1">{{ choice.optionId }}</p>
     </div>
   </div>
